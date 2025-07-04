688e4fdeb825f0ea59d662609b321577c1b6aa0a
ArcanaDoble <arcanadoble@gmail.com>
2025-07-04 04:10:35 +0200

    Add draggable tokens for battle map

--- src/components/AssetSidebar.jsx
+++ src/components/AssetSidebar.jsx
@@ -3,8 +3,11 @@ import PropTypes from 'prop-types';
 import { nanoid } from 'nanoid';
 import { FiChevronDown, FiChevronRight, FiTrash } from 'react-icons/fi';
 import { motion, AnimatePresence } from 'framer-motion';
+import { useDrag } from 'react-dnd';
 
-const AssetSidebar = ({ onAssetSelect }) => {
+export const AssetTypes = { IMAGE: 'asset-image' };
+
+const AssetSidebar = ({ onAssetSelect, onDragStart }) => {
   const [folders, setFolders] = useState(() => [
     { id: nanoid(), name: 'Enemigos', assets: [], open: true },
   ]);
@@ -136,28 +139,41 @@ const AssetSidebar = ({ onAssetSelect }) => {
                       className="text-sm w-full"
                     />
                     <div className="grid grid-cols-4 gap-2">
-                      {folder.assets.map((asset) => (
-                        <div key={asset.id} className="text-center text-xs">
-                          <div className="relative group">
-                            <img
-                              src={asset.url}
-                              alt={asset.name}
-                              className="w-16 h-16 object-contain rounded cursor-pointer hover:ring-2 hover:ring-blue-500"
-                              onClick={() => onAssetSelect?.(asset)}
-                              onMouseEnter={(e) => showPreview(asset, e)}
-                              onMouseMove={movePreview}
-                              onMouseLeave={hidePreview}
-                            />
-                            <button
-                              onClick={() => removeAsset(folder.id, asset.id)}
-                              className="absolute -top-1 -right-1 bg-gray-800 rounded-full p-0.5 text-gray-300 opacity-0 group-hover:opacity-100 hover:text-red-400"
-                            >
-                              <FiTrash />
-                            </button>
+                      {folder.assets.map((asset) => {
+                        const [{ isDragging }, drag] = useDrag(
+                          () => ({
+                            type: AssetTypes.IMAGE,
+                            item: { url: asset.url, name: asset.name },
+                            collect: (monitor) => ({
+                              isDragging: monitor.isDragging(),
+                            }),
+                            begin: () => onDragStart?.({ url: asset.url, name: asset.name }),
+                          }),
+                          [asset, onDragStart]
+                        );
+                        return (
+                          <div key={asset.id} className="text-center text-xs">
+                            <div ref={drag} className="relative group" style={{ opacity: isDragging ? 0.5 : 1 }}>
+                              <img
+                                src={asset.url}
+                                alt={asset.name}
+                                className="w-16 h-16 object-contain rounded cursor-pointer hover:ring-2 hover:ring-blue-500"
+                                onClick={() => onAssetSelect?.(asset)}
+                                onMouseEnter={(e) => showPreview(asset, e)}
+                                onMouseMove={movePreview}
+                                onMouseLeave={hidePreview}
+                              />
+                              <button
+                                onClick={() => removeAsset(folder.id, asset.id)}
+                                className="absolute -top-1 -right-1 bg-gray-800 rounded-full p-0.5 text-gray-300 opacity-0 group-hover:opacity-100 hover:text-red-400"
+                              >
+                                <FiTrash />
+                              </button>
+                            </div>
+                            <span className="truncate block w-16 mx-auto">{asset.name}</span>
                           </div>
-                          <span className="truncate block w-16 mx-auto">{asset.name}</span>
-                        </div>
-                      ))}
+                        );
+                      })}
                     </div>
                   </motion.div>
                 )}
@@ -184,6 +200,7 @@ const AssetSidebar = ({ onAssetSelect }) => {
 
 AssetSidebar.propTypes = {
   onAssetSelect: PropTypes.func,
+  onDragStart: PropTypes.func,
 };
 
 export default AssetSidebar;
